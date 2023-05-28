1803a99cfd587e15d01feadf6a8cf93953f60420
DryWinder <drywinder@gmail.com>
2023-05-28 19:54:42 +0300

    commit

--- flask/SQLA_config.py
+++ flask/SQLA_config.py
@@ -26,14 +26,13 @@ Tests = Base.classes.tests_results
 conn = engine.connect()
 #result = conn.execute(query)
 
-
-def fetchRowsFromStudents():
-    query = select(Students).limit(10)
-    return conn.execute(query)
+"""
+Methods for Locations table
+"""
 
 
 def fetchRowsFromLocations():
-    query = select(Locations).limit(10)
+    query = select(Locations).order_by(Locations.location_id).limit(10)
     return conn.execute(query)
 
 
@@ -42,19 +41,158 @@ def fetchLocationsById(location_id):
     return query.first()
 
 
-def fetchRowsFromEO():
-    query = select(EO).limit(10)
-    return conn.execute(query)
-
-
-def fetchRowsFromTests():
-    query = select(Tests).limit(10)
-    return conn.execute(query)
-
-
 def fetchRegnames():
     query = select(distinct(Locations.regname))
     return conn.execute(query)
+
+
+def fetchLocation(regname, areaname, tername, tertypename):
+    query = session.query(Locations).filter(
+        Locations.regname == regname,
+        Locations.areaname == areaname,
+        Locations.tername == tername,
+        Locations.tertypename == tertypename
+    )
+    return query.first()
+
+
+def deleteLocation(location_id):
+    location = fetchLocationsById(location_id)
+    if location:
+        session.delete(location)
+        session.commit()
+
+
+def updateLocation(location_id, regname, areaname, tername, tertypename):
+    location = fetchLocationsById(location_id)
+    if location:
+        location.regname = regname
+        location.areaname = areaname
+        location.tername = tername
+        location.tertypename = tertypename
+        session.commit()
+
+
+"""
+Methods for Students table
+"""
+
+
+def fetchRowsFromStudents():
+    query = select(Students).order_by(Students.student_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchStudentById(student_id):
+    query = session.query(Students).filter(Students.student_id == student_id)
+    return query.first()
+
+
+def createStudent(data):
+    student = Students(year_of_passing=data['year_of_passing'], outid=data['outid'], birth=data['birth'],
+                       sextypename=data['sextypename'], location_id=data['location_id'], eo_id=data['eo_id'])
+    session.add(student)
+    session.commit()
+
+
+def updateStudent(student_id, year_of_passing, outid, birth, sextypename, location_id, eo_id, tests_results_id):
+    student = fetchStudentById(student_id)
+    if student:
+        student.year_of_passing = year_of_passing
+        student.outid = outid
+        student.birth = birth
+        student.sextypename = sextypename
+        student.location_id = location_id
+        student.eo_id = eo_id
+        student.tests_results_id = tests_results_id
+        session.commit()
+
+def deleteStudent(outid):
+    student = fetchStudentById(outid)
+    if student:
+        session.delete(student)
+        session.commit()
+
+
+"""
+Methods for Educational organizations table
+"""
+
+
+def fetchRowsFromEO():
+    query = select(EO).order_by(EO.eo_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchEOById(eo_id):
+    query = session.query(EO).filter(EO.eo_id == eo_id)
+    return query.first()
+
+
+def fetchEO(eo_name, eo_type, location_id):
+    query = session.query(EO).filter(
+        EO.eo_name == eo_name,
+        EO.eo_type == eo_type,
+        EO.location_id == location_id
+    )
+    return query.first()
+
+
+def deleteEO(eo_name, eo_type, location_id):
+    eo = fetchEO(eo_name, eo_type, location_id)
+    if eo:
+        session.delete(eo)
+        session.commit()
+
+
+def updateEO(eo_id, eo_name, eo_type, location_id):
+    eo = fetchEOById(eo_id)
+    if eo:
+        eo.eo_name = eo_name
+        eo.eo_type = eo_type
+        eo.location_id = location_id
+        session.commit()
+
+
+"""
+Methods for Tests Results table
+"""
+
+
+def fetchRowsFromTests():
+    query = select(Tests).order_by(Tests.tests_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchTestsColumnNames():
+    return Tests.__table__.columns.keys()
+
+
+def fetchTest(tests_id):
+    query = session.query(Tests).filter(Tests.tests_id == tests_id)
+    return query.first()
+
+
+def updateTest(tests_id, test_data):
+    test = fetchTest(tests_id)
+    if test:
+        for column_name, value in test_data.items():
+            if value == 'None':
+                value = None
+            setattr(test, column_name, value)
+        session.commit()
+
+
+def deleteTest(student_id):
+    test = fetchTest(student_id)
+    if test:
+        session.delete(test)
+        session.commit()
+
+
+"""
+Different queries
+"""
 
 
 def fetchGradesByYearAndRegion():
@@ -96,43 +234,6 @@ def fetchGrade(year, regname, subject, function):
 
     return query.all()
 
-
-def fetchEOById(eo_id):
-    query = session.query(EO).filter(EO.eo_id == eo_id)
-    return query.first()
-
-
-def fetchTestsColumnNames():
-    return Tests.__table__.columns.keys()
-
-def fetchStudentById(student_id):
-    query = session.query(Students).filter(Students.student_id == student_id)
-    return query.first()
-
-
-def createStudent(data):
-    student = Students(year_of_passing=data['year_of_passing'], outid=data['outid'], birth=data['birth'],
-                       sextypename=data['sextypename'], location_id=data['location_id'], eo_id=data['eo_id'])
-    session.add(student)
-    session.commit()
-
-
-def updateStudent(student_id, data):
-    student = fetchStudentById(student_id)
-    if student:
-        student.year_of_passing = data['year_of_passing']
-        student.outid = data['outid']
-        student.birth = data['birth']
-        student.sextypename = data['sextypename']
-        student.location_id = data['location_id']
-        student.eo_id = data['eo_id']
-        session.commit()
-
-def deleteStudent(student_id):
-    student = fetchStudentById(student_id)
-    if student:
-        session.delete(student)
-        session.commit()
 
 def subjectDict():
     return {"Українська мова та література": "uml_test_ball100",
--- flask/app.py
+++ flask/app.py
@@ -51,9 +51,6 @@ def showTables():
 
             return render_template("filters.html", years=years, regnames=regnames, subjects=subjects, functions=functions, url="/filters")
 
-        elif request.form["update_delete"] == "Delete":
-            pass
-
     return render_template("main.html")
 
 
@@ -63,17 +60,26 @@ def locationsTable():
     headers = ("location_id", "regname", "areaname", "tername", "tertypename")
     data = tuple(fetchRowsFromLocations())
     if request.method == 'POST':
+        location_id = request.form['location_id']
         regname = request.form['regname']
         areaname = request.form['areaname']
         tername = request.form['tername']
         tertypename = request.form['tertypename']
-        location = Locations(regname=regname, areaname=areaname, tername=tername, tertypename=tertypename)
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                updateLocation(location_id, regname, areaname, tername, tertypename)
+                print(request.form)
+            if request.form['update_delete'] == "Delete":
+                deleteLocation(location_id)
 
-        session.add(location)
+        else:
+            location = Locations(regname=regname, areaname=areaname, tername=tername, tertypename=tertypename)
+            session.add(location)
         session.commit()
-        return redirect("/")
+        return redirect(url)
 
     return render_template("table.html", headers=headers, data=data, url=url)
+
 
 @app.route("/eo", methods=['POST', 'GET'])
 def eoTable():
@@ -81,23 +87,35 @@ def eoTable():
     headers = ("eo_id", "eo_name", "eo_type", "location_id")
     data = tuple(fetchRowsFromEO())
     if request.method == 'POST':
+        eo_id = request.form['eo_id']
         eo_name = request.form['eo_name']
         eo_type = request.form['eo_type']
         location_id = request.form['location_id']
-        eo = EO(eo_name=eo_name, eo_type=eo_type, location_id=location_id)
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                updateEO(eo_id, eo_name, eo_type, location_id)
 
-        session.add(eo)
+            if request.form['update_delete'] == "Delete":
+                deleteEO(eo_name, eo_type, location_id)
+
+        else:
+            eo = EO(eo_name=eo_name, eo_type=eo_type, location_id=location_id)
+            session.add(eo)
+
         session.commit()
-        return redirect("/")
+
+        return redirect(url)
 
     return render_template("table.html", headers=headers, data=data, url=url)
+
 
 @app.route("/students", methods=['POST', 'GET'])
 def studentsTable():
     url = "/students"
-    headers = ("student_id", "year_of_passing", "outid", "birth", "sextypename", "location_id", "eo_id")
+    headers = ("student_id", "year_of_passing", "outid", "birth", "sextypename", "location_id", "eo_id", "tests_results_id")
     data = tuple(fetchRowsFromStudents())
     if request.method == 'POST':
+        student_id = request.form['student_id']
         year_of_passing = request.form['year_of_passing']
         outid = request.form['outid']
         birth = request.form['birth']
@@ -105,11 +123,20 @@ def studentsTable():
         location_id = request.form['location_id']
         eo_id = request.form['eo_id']
         tests_results_id = request.form['tests_results_id']
-        student = Students(year_of_passing=year_of_passing, outid=outid, birth=birth, sextypename=sextypename, location_id=location_id, eo_id=eo_id, tests_results_id=tests_results_id)
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                updateStudent(student_id, year_of_passing, outid, birth, sextypename, location_id, eo_id, tests_results_id)
 
-        session.add(student)
+            if request.form['update_delete'] == "Delete":
+                deleteStudent(outid)
+                session.commit()
+
+        else:
+            student = Students(year_of_passing=year_of_passing, outid=outid, birth=birth, sextypename=sextypename, location_id=location_id, eo_id=eo_id, tests_results_id=tests_results_id)
+            session.add(student)
+
         session.commit()
-        return redirect("/")
+        return redirect(url)
 
     return render_template("table.html", headers=headers, data=data, url=url)
 
@@ -122,10 +149,17 @@ def testsTable():
     if request.method == 'POST':
         tests_data = {column_name: request.form.get(column_name) if request.form.get(column_name) != '' else None for column_name in headers}
         test = Tests(**tests_data)
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                updateTest(test.tests_id, tests_data)
 
-        session.add(test)
+            if request.form['update_delete'] == "Delete":
+                deleteTest(test.student_id)
+
+        else:
+            session.add(test)
         session.commit()
-        return redirect("/")
+        return redirect(url)
 
     return render_template("table.html", headers=headers, data=data, url=url)
 
@@ -167,5 +201,17 @@ def filters():
 
     return render_template("filters.html", years=years, regnames=regnames, subjects=subjects, functions=functions, grade=grade, url="/filters")
 
+"""
+@app.route("/update_delete", methods=["POST", "GET"])
+def update_delete():
+    if request.method == 'POST':
+        if request.form["update_delete"] == "Update":
+            print("Update")
+
+        if request.form["update_delete"] == "Delete":
+            print("Delete")
+
+        return redirect("/")
+"""
 if __name__ == "__main__":
     app.run(debug=True)
